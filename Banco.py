f8399a995421328e2bd9bf2c587bb3947e1a1344
JoseAntonioIosephvsAnthonivs <jojose.a8@gmail.com>
2021-06-17 19:44:20 -0300

    Add files via upload

---
+++ Banco.py
@@ -0,0 +1,35 @@
+import sqlite3
+class Banco():
+    def __init__(self):
+        self.conexao = sqlite3.connect('banco.db')
+        self.createTable()
+
+    def createTable(self):
+        c = self.conexao.cursor()
+
+        c.execute("""create table if not exists usuarios (
+                     idusuario integer primary key autoincrement ,
+                     nome text,
+                     telefone text,
+                     email text,
+                     usuario text,
+                     senha text)""")
+        self.conexao.commit()
+        c.close()
+#
+# class Banco():
+#
+#     def __init__(self):
+#         self.conexao = sqlite3.connect('banco.db')
+#         self.createTable()
+#
+#     def crateTable(self):
+#         c = self.conexao.cursor()
+#         c.execute("""create table if not exists usuarios (
+#                      idusuario integer primary key autoincrement ,
+#                      nome text,
+#                      telefone text,
+#                      email text,
+#                      senha text)""")
+#         self.conexao.commit()
+#         c.close()
